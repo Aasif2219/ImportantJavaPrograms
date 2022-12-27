bcee2d3deef1b81e70cad0436595f9bbb6589029
Aasif <aasifshaikh2205@gmail.com>
2022-12-27 17:27:08 +0530

    Commit P02

---
+++ ImportantJavaPrograms/src/ReverseNumbers.java
@@ -0,0 +1,32 @@
+
+public class ReverseNumbers {
+
+	public static void main(String[] args) {
+
+     int num=1234;
+     
+     //Method 1 - Normal method
+    /* int rev=0;
+     
+     while(num!=0) {
+    	 
+    	 rev=rev*10+num%10;
+    	 num=num/10;
+    	 
+     }
+     System.out.println(rev);  
+
+     //Method 2 - Using StringBuilder
+     StringBuilder str= new StringBuilder();
+     str.append(num);
+     StringBuilder rev1=str.reverse();
+     System.out.println(rev1); */
+     
+     //Method 3 - Using StringBuffer
+     StringBuffer sb= new StringBuffer(String.valueOf(num));
+     StringBuffer rev=sb.reverse();
+     System.out.println(rev);
+     
+	}
+
+}
